5b9bd9a18628927cd0772887efbc248a517cb631
adu <515526080@qq.com>
2025-12-06 21:56:40 +0800

    此编译用于调试

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 windres resource.rc -o resource.o
-gcc -Wall -Wextra -std=c99 -finput-charset=UTF-8 -fexec-charset=GBK -Os -s -o ech-win-gui.exe resource.o main.c -luser32 -lkernel32 -lcomctl32 -lgdi32 -mwindows
+gcc -Wall -Wextra -std=c99 -finput-charset=UTF-8 -fexec-charset=GBK -g -gdwarf-2 -O0 -o ech-win-gui.exe resource.o main.c -luser32 -lkernel32 -lcomctl32 -lgdi32 -mwindows
 pause
