652df426aff29baa5ecdba5f15466a9bb6a8e5f5
adu <515526080@qq.com>
2025-12-17 23:56:22 +0800

    此编译用于调试

--- build.bat
+++ build.bat
@@ -1,3 +1,10 @@
-windres resource.rc -o resource.o
-gcc -Wall -Wextra -std=c99 -finput-charset=UTF-8 -fexec-charset=GBK -g -gdwarf-2 -O0 -o ech-win-gui.exe resource.o main.c -luser32 -lkernel32 -lcomctl32 -lgdi32 -mwindows
+@echo off
+echo Compiling...
+windres resource.rc -o .temp.o && gcc -Wall -Wextra -std=c99 -finput-charset=UTF-8 -fexec-charset=GBK -g -gdwarf-2 -O0 -o ech-win-gui.exe main.c .temp.o -luser32 -lkernel32 -lcomctl32 -lgdi32 -mwindows && del .temp.o
+
+if exist ech-win-gui.exe (
+    echo Compilation successful!
+) else (
+    echo Compilation failed!
+)
 pause
